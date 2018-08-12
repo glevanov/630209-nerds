8c83fb3ca27c793694e0891834a701c6205cf438
Grigoriy Levanov <glevanov@gmail.com>
2018-08-12 12:07:22 +0300

    Инициализация проекта

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const browserSync = require('browser-sync').create();
+
+gulp.task('images', function(){
+  return gulp.src(['app/**/*.+(png|jpg|gif|svg)', '!app/guide/preview/*'])
+    .pipe(imagemin())
+    .pipe(gulp.dest('img'))
+});
+
+gulp.task('styles', function() {
+  return gulp.src('app/blocks/**/*.+(css)')
+    .pipe(concat('main.css'))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('app/blocks/**/*.+(css)', ['styles'])
+});
